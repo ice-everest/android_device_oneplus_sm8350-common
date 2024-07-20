425556c06f8d4f4e8ddc32964aa5f8e836976e58
mvimal2607 <mvimal2607@gmail.com>
2024-07-20 14:48:31 +0530

    sm8350-common: Add some more packages to removed packages
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,14 +5,25 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AiWallpapers \
+    AmbientSensePrebuilt \
+    AndroidAutoStubPrebuilt \
+    arcore \
+    CalculatorGooglePrebuilt \
     Chrome \
     Chrome-Stub \
     Drive \
+    FilesPrebuilt \
+    Glimpse \
+    GoogleTTS \
+    HealthIntelligenceStubPrebuilt \
     Maps \
     PrebuiltGmail \
+    Recorder \
     RecorderPrebuilt \
     SafetyHubPrebuilt \
     Videos \
+    WallpaperEmojiPrebuilt \
     YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
